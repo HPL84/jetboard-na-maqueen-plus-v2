84853f5a9f545dd576a705829d07121be5e5d8cb
HPL84 <ghapel@gmail.com>
2024-10-22 10:54:03 +0200

    Aktualizacja main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+def on_button_pressed_ab():
+    global hpl
+    if hpl == 0:
+        hpl += 1
+        basic.show_icon(IconNames.HEART)
+    else:
+        hpl += -1
+        maqueenPlusV2.control_motor_stop(maqueenPlusV2.MyEnumMotor.LEFT_MOTOR)
+        maqueenPlusV2.control_motor_stop(maqueenPlusV2.MyEnumMotor.RIGHT_MOTOR)
+        basic.clear_screen()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+P = 0
+hpl = 0
 DFRobotMaqueenPlusV2.init()
+
+def on_forever():
+    global P
+    P = maqueenPlusV2.read_ultrasonic(DigitalPin.P13, DigitalPin.P14)
+    basic.show_number(P)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,12 +1,22 @@
-input.onButtonPressed(Button.A, function () {
-    if (hpl == 0) {
-        hpl += 1
-        basic.showIcon(IconNames.Heart)
+input.onButtonPressed(Button.AB, function () {
+	
+})
+let P = 0
+DFRobotMaqueenPlusV2.init()
+basic.forever(function () {
+    P = sonar.ping(
+    DigitalPin.P13,
+    DigitalPin.P14,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(P)
+    if (P <= 15) {
+        maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Green)
+    } else if (P >= 16 && P < 30) {
+        maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Blue)
+    } else if (P >= 31 && P < 50) {
+        maqueenPlusV2.showColor(maqueenPlusV2.NeoPixelColors.Red)
     } else {
-        hpl += -1
-        basic.clearScreen()
+    	
     }
 })
-let hpl = 0
-DFRobotMaqueenPlusV2.init()
-basic.clearScreen()
